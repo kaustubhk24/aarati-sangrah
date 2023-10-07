a7687854d76f08d37e4505f49df4f4db2e3bd52b
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2023-10-07 21:32:46 +0530

    additions

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Hello() {
         <img src='/img/pwa/192X192.png'/>
 
 <h5>नवरात्र घटस्थापना शास्त्रोक्त पुजेसाठी <a href='ghat-sthapana-puja'>येथे क्लिक करा</a> </h5>
-
+<h5>नवरात्र - देवीच्या आरत्यांसाठी <a href='devi'>येथे क्लिक करा</a> </h5>
         <div className="container">
         <ProjDevCardsRow/>
         </div>
